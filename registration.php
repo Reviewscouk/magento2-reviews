2b5948595b68014b5baee249122c7ae89cefd946
Tom Foyster <tomfoyster@gmail.com>
2016-07-07 11:25:30 +0100

    Added module XML declaration and corrected module name in reg file

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Reviewscouk_reviews',
+    'Reviewscouk_Reviews',
     __DIR__
 );

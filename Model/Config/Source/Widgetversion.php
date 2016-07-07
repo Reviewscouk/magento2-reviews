fa02cb7e8d6a4146550e5f3ac795efa858e9d156
Tom Foyster <tomfoyster@gmail.com>
2016-07-07 14:46:37 +0100

    Added new source model for widget version

---
+++ Model/Config/Source/Widgetversion.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Reviewscouk\Reviews\Model\Config\Source;
+
+use Magento\Framework\App\ObjectManager;
+
+class Widgetversion implements \Magento\Framework\Option\ArrayInterface {
+
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return array(
+            array('value' => '1', 'label'=>__('Javascript Widget')),
+            array('value' => '2', 'label'=>__('Static Content Widget')),
+        );
+    }
+
+    /**
+     * Get options in "key-value" format
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return array(
+            '1' => __('V1 - Javascript Widget'),
+            '2' => __('V2 - Static Content'),
+        );
+    }
+
+}

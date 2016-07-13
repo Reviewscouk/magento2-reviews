9eee6d16bba9f4d8eace1bd00dd9aa3f847e96c0
Tom Foyster <tomfoyster@gmail.com>
2016-07-13 10:22:30 +0100

    Added block to display Sync Reviews button

---
+++ Block/Adminhtml/System/Config/SyncReviews.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Reviewscouk\Reviews;
+
+use Magento\Config as Config;
+
+class SyncReviews extends Config\Block\System\Config\Form\Field
+{
+
+    protected $_buttonLabel = 'Sync Product Reviews';
+
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        if (!$this->getTemplate()) {
+            $this->setTemplate('system/config/syncreviews.phtml');
+        }
+        return $this;
+    }
+
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+        return parent::render($element);
+    }
+
+    public function setButtonLabel($buttonLabel)
+    {
+        $this->_buttonLabel = $buttonLabel;
+        return $this;
+    }
+
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $originalData = $element->getOriginalData();
+        $buttonLabel = !empty($originalData['button_label']) ? $originalData['button_label'] : $this->_vatButtonLabel;
+        $this->addData(
+            [
+                'button_label' => __($buttonLabel),
+                'html_id' => $element->getHtmlId(),
+                'ajax_url' => $this->_urlBuilder->getUrl('customer/system_config/syncreviews'),
+            ]
+        );
+
+        return $this->_toHtml();
+    }
+}

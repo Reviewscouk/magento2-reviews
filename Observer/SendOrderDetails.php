aaa0a9f9a7f70b147554907ec6a7fce7c6f3c510
Tom Foyster <tomfoyster@gmail.com>
2016-07-25 11:36:37 +0100

    Removed underscores from class vars; Removed unneeded DI and params

--- Observer/SendOrderDetails.php
+++ Observer/SendOrderDetails.php
@@ -11,27 +11,24 @@ use Magento\Store as Store;
 class SendOrderDetails implements Framework\Event\ObserverInterface
 {
 
-    private $_configHelper;
-    private $_apiModel;
-    private $_productModel;
-    private $_imageHelper;
-    private $_configProductModel;
-    private $_store;
+    private $configHelper;
+    private $apiModel;
+    private $productModel;
+    private $imageHelper;
+    private $configProductModel;
 
     public function __construct(
         Reviews\Helper\Config $config,
         Reviews\Model\Api $api,
         Catalog\Model\Product $product,
         Catalog\Helper\Image $image,
-        ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable $configurable,
-        Store\Model\StoreManagerInterface $storeManagerInterface
+        ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable $configurable
     ) {
-        $this->_configHelper = $config;
-        $this->_apiModel = $api;
-        $this->_productModel = $product;
-        $this->_imageHelper = $image;
-        $this->_configProductModel = $configurable;
-        $this->_store = $storeManagerInterface->getStore();
+        $this->configHelper = $config;
+        $this->apiModel = $api;
+        $this->productModel = $product;
+        $this->imageHelper = $image;
+        $this->configProductModel = $configurable;
     }
 
     public function execute(Framework\Event\Observer $observer)
@@ -46,8 +43,8 @@ class SendOrderDetails implements Framework\Event\ObserverInterface
         try {
             $magento_store_id = $order->getStoreId();
 
-            if ($this->_configHelper->getStoreId($magento_store_id) && $this->_configHelper->getApiKey($magento_store_id) && $this->_configHelper->isMerchantReviewsEnabled($magento_store_id)) {
-                $merchantResponse = $this->_apiModel->apiPost(
+            if ($this->configHelper->getStoreId($magento_store_id) && $this->configHelper->getApiKey($magento_store_id) && $this->configHelper->isMerchantReviewsEnabled($magento_store_id)) {
+                $merchantResponse = $this->apiModel->apiPost(
                     'merchant/invitation',
                     [
                         'source' => 'magento',
@@ -57,22 +54,22 @@ class SendOrderDetails implements Framework\Event\ObserverInterface
                     ],
                     $magento_store_id
                 );
-                $this->_apiModel->addStatusMessage($merchantResponse, "Merchant Review Invitation");
+                $this->apiModel->addStatusMessage($merchantResponse, "Merchant Review Invitation");
             }
 
-            if ($this->_configHelper->getStoreId($magento_store_id) && $this->_configHelper->getApiKey($magento_store_id) && $this->_configHelper->isProductReviewsEnabled($magento_store_id)) {
+            if ($this->configHelper->getStoreId($magento_store_id) && $this->configHelper->getApiKey($magento_store_id) && $this->configHelper->isProductReviewsEnabled($magento_store_id)) {
                 $items = $order->getAllVisibleItems();
                 foreach ($items as $item) {
-                    $item = $this->_productModel->load($item->getProductId());
+                    $item = $this->productModel->load($item->getProductId());
 
-                    if ($this->_configHelper->isUsingGroupSkus($magento_store_id)) {
+                    if ($this->configHelper->isUsingGroupSkus($magento_store_id)) {
                         // If product is part of a grouped product, use the grouped product details.
-                        $parentIds = $this->_configProductModel->getParentIdsByChild($item->getId());
+                        $parentIds = $this->configProductModel->getParentIdsByChild($item->getId());
                         if (!empty($parentIds)) {
-                            $item = $this->_productModel->load($parentIds[0]);
+                            $item = $this->productModel->load($parentIds[0]);
                         }
                     }
-                    $imageUrl = $this->_imageHelper->init($item, 'product_page_image_large')->getUrl();
+                    $imageUrl = $this->imageHelper->init($item, 'product_page_image_large')->getUrl();
                     $p = [
                         'image' => $imageUrl,
                         'id' => $item->getProductId(),
@@ -82,7 +79,7 @@ class SendOrderDetails implements Framework\Event\ObserverInterface
                     ];
                 }
 
-                $productResponse = $this->_apiModel->apiPost(
+                $productResponse = $this->apiModel->apiPost(
                     'product/invitation',
                     [
                         'source' => 'magento',
@@ -93,7 +90,7 @@ class SendOrderDetails implements Framework\Event\ObserverInterface
                     ],
                     $magento_store_id
                 );
-                $this->_apiModel->addStatusMessage($productResponse, "Product Review Invitation");
+                $this->apiModel->addStatusMessage($productResponse, "Product Review Invitation");
 
             }
         } catch (\Exception $e) {

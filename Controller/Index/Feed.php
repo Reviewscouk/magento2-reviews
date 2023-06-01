fb9e93c5c62d726faada6a80af448d369be5507e
ajay2967 <ajay@reviews.io>
2023-06-01 15:00:01 +0100

    Add csv product feed

--- Controller/Index/Feed.php
+++ Controller/Index/Feed.php
@@ -7,8 +7,11 @@ use Magento\Catalog as Catalog;
 use Magento\CatalogInventory as CatalogInventory;
 use Magento\Store as Store;
 use Reviewscouk\Reviews as Reviews;
+use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
+use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
+use Magento\Framework\App\Filesystem\DirectoryList;
 
-class Feed extends Framework\App\Action\Action
+class Feed implements HttpGetActionInterface
 {
 
     private $configHelper;
@@ -17,19 +20,28 @@ class Feed extends Framework\App\Action\Action
     private $stockModel;
     private $imageHelper;
     private $storeModel;
+    private $productCollectionFactory;
+    private $directory;
+    protected $_fileCsv;
 
     public function __construct(
-        Framework\App\Action\Context $context,
+        //Framework\App\Action\Context $context,
         Framework\Cache\Core $core,
         Catalog\Model\Product $product,
         CatalogInventory\Api\StockRegistryInterface $stockRegistryInterface,
         Catalog\Helper\Image $image,
         Store\Model\StoreManagerInterface $storeManagerInterface,
         Reviews\Helper\Config $config,
-        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+        //\Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+        \Magento\Framework\Filesystem $filesystem,
+        \Magento\Framework\Module\Dir\Reader $moduleReader,
+        \Magento\Framework\File\Csv $fileCsv,
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+        \Magento\Catalog\Model\CategoryFactory $categoryFactory,
      )
     {
-        parent::__construct($context);
+        // parent::__construct($context);
 
         $this->configHelper = $config;
         $this->cache = $core;
@@ -38,6 +50,11 @@ class Feed extends Framework\App\Action\Action
         $this->imageHelper = $image;
         $this->storeModel = $storeManagerInterface;
         $this->productCollectionFactory = $productCollectionFactory;
+        $this->directory = $filesystem->getDirectoryWrite(DirectoryList::VAR_DIR);
+        $this->_moduleReader = $moduleReader;
+        $this->_fileCsv = $fileCsv;
+        $this->resultJsonFactory = $resultJsonFactory;
+        $this->_categoryFactory = $categoryFactory;
     }
 
     private function getProductCollection()
@@ -53,6 +70,13 @@ class Feed extends Framework\App\Action\Action
             return $collection;
     }
 
+    public function getCategoryName($categoryId)
+    {
+        $category = $this->_categoryFactory->create()->load($categoryId);
+        $categoryName = $category->getName();
+        return $categoryName;
+    }
+
     public function execute()
     {
         // Set timelimit to 0 to avoid timeouts when generating feed.
@@ -64,68 +88,100 @@ class Feed extends Framework\App\Action\Action
         $productFeedEnabled = $this->configHelper->isProductFeedEnabled($store->getId());
         if ($productFeedEnabled) {
             // TODO:- Implement caching of Feed
-            $productFeed = "<?xml version='1.0'?>
-                    <rss version ='2.0' xmlns:g='http://base.google.com/ns/1.0'>
-                    <channel>
-                    <title><![CDATA[" . $store->getName() . "]]></title>
-                    <link>" . $store->getBaseUrl() . "</link>";
 
             $products = $this->getProductCollection();
 
+            // $productArrayHeader[] = array('id', 'title', 'link', 'price', 'sale_price', 'description', 'condition', 'image_link', 'brand', 'mpn', 'gtin', 'product_type', 'shipping_country', 'shipping_service', 'shipping_price', 'availability');
+            $productArrayHeader = ['id', 'title', 'link', 'price', 'sale_price', 'description', 'condition', 'image_link', 'brand', 'mpn', 'gtin', 'product_type', 'shipping_country', 'shipping_service', 'shipping_price', 'availability', 'google_product_category', 'custom_attribute'];
+
+
+            $filepath = 'export/list.csv';
+            $this->directory->create('export');
+            $stream = $this->directory->openFile($filepath, 'w+');
+            $stream->lock();
+            $header = $productArrayHeader;
+            $stream->writeCsv($header);
+
+
+
             foreach ($products as $product) {
                 // Load image url via helper.
-                $imageUrl = $this->imageHelper->init($product, 'product_page_image_large')->getUrl();
-
-                $brand = $product->hasData('manufacturer') ? $product->getAttributeText('manufacturer') : ($product->hasData('brand') ? $product->getAttributeText('brand') : 'Not Available');
+                $imageUrl = $this->imageHelper->init($product, 'product_page_image_large')->getUrl();                
                 $price = $product->getPrice();
-
                 $finalPrice = $product->getFinalPrice();
-
-                $productFeed .= "<item>
-                        <g:id><![CDATA[" . $product->getSku() . "]]></g:id>
-                        <title><![CDATA[" . $product->getName() . "]]></title>
-                        <link><![CDATA[" . $product->getProductUrl() . "]]></link>
-                        <g:price>" . number_format($price, 2) . " " . $store->getCurrentCurrency()->getCode() . "</g:price>
-                        <g:sale_price>" . number_format($finalPrice, 2) . " " . $store->getCurrentCurrency()->getCode() . "</g:sale_price>
-                        <description><![CDATA[]]></description>
-                        <g:condition>new</g:condition>
-                        <g:image_link><![CDATA[" . $imageUrl . "]]></g:image_link>
-                        <g:brand><![CDATA[" . $brand . "]]></g:brand>
-                        <g:mpn><![CDATA[" . ($product->hasData('mpn') ? $product->getData('mpn') : $product->getSku()). "]]></g:mpn>
-                        <g:gtin><![CDATA[" . ($product->hasData('gtin') ? $product->getData('gtin') : ($product->hasData('upc') ? $product->getData('upc') : '')). "]]></g:gtin>
-                        <g:product_type><![CDATA[" . $product->getTypeID() . "]]></g:product_type>
-                        <g:shipping>
-                        <g:country>UK</g:country>
-                        <g:service>Standard Free Shipping</g:service>
-                        <g:price>0 GBP</g:price>
-                        </g:shipping>";
-
-                $categoryCollection = $product->getCategoryCollection();
-                if (count($categoryCollection) > 0) {
-                    foreach ($categoryCollection as $category) {
-                        $productFeed .= "<g:google_product_category><![CDATA[" . $category->getName() . "]]></g:google_product_category>";
-                    }
-                }
+                $brand = $product->hasData('manufacturer') ? $product->getAttributeText('manufacturer') : ($product->hasData('brand') ? $product->getAttributeText('brand') : 'Not Available');
 
                 $stock = $this->stockModel->getStockItem(
                     $product->getId(),
                     $product->getStore()->getWebsiteId()
                 );
+                $stockAvailibility = "out of stock";
                 if ($stock->getIsInStock()) {
-                    $productFeed .= "<g:availability>in stock</g:availability>";
-                } else {
-                    $productFeed .= "<g:availability>out of stock</g:availability>";
+                    $stockAvailibility = "in stock";
                 }
 
-                $productFeed .= "</item>";
-            }
+                // Add the parent product
+                $productArrayBody = [
+                    $product->getSku(),
+                    $product->getName(),
+                    $product->getProductUrl(),
+                    (!empty($price) ? number_format($price, 2) . " " . $store->getCurrentCurrency()->getCode() : ''),
+                    (!empty($finalPrice) ? number_format($finalPrice, 2) . " " . $store->getCurrentCurrency()->getCode() : ''),
+                    "",
+                    "new",
+                    $imageUrl,
+                    $brand,
+                    ($product->hasData('mpn') ? $product->getData('mpn') : $product->getSku()),
+                    ($product->hasData('gtin') ? $product->getData('gtin') : ($product->hasData('upc') ? $product->getData('upc') : '')),
+                    $product->getTypeID(),
+                    "UK",
+                    "Standard Free Shipping",
+                    "0 GBP",
+                    $stockAvailibility
+                ];
 
-            $productFeed .= "</channel></rss>";
+                $categoryIds = $product->getCategoryIds();
+                $categoryNames = [];
+                foreach ($categoryIds as $categoryId) {
+                    array_push($categoryNames, $this->getCategoryName($categoryId));
+                }
+                $productArrayBody[] = implode(', ', $categoryNames);
+
+
+
+
+                $attributes = $product->getAttributes();
+                $attributeNames = [];
+                // foreach($attributes as $attribute) {
+                //     $attributeNames[] = $attribute->getName();
+                // }
+                // $productArrayBody[] = implode(', ', $attributeNames);
+                
+                foreach ($attributes as $attribute) { 
+                    $attributeNames[] = $attribute->getFrontend()->getLabel();
+                }
+                $productArrayBody[] = implode(', ', $attributeNames);
+                print_r($attributeNames);
+                die();
+
+
+
+                $stream->writeCsv($productArrayBody);
+            }
 
             // TODO:- Implement caching of feed
 
-            print $productFeed;
             // exit();
+
+            
+            $csvContents = array_merge($productArrayHeader, $productArrayBody);
+            print_r($productArrayHeader);
+            print_r($productArrayBody);
+
+            $resultJson = $this->resultJsonFactory->create();
+            return $resultJson->setData(['data' => $csvContents]);
+
+
         } else {
             print "Product Feed is disabled.";
         }
---
+++ Controller/Index/Feed_xml.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Reviewscouk\Reviews\Controller\Index;
+
+use Magento\Framework as Framework;
+use Magento\Catalog as Catalog;
+use Magento\CatalogInventory as CatalogInventory;
+use Magento\Store as Store;
+use Reviewscouk\Reviews as Reviews;
+use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
+use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
+
+class Feed implements HttpGetActionInterface
+{
+
+    private $configHelper;
+    private $cache;
+    private $productModel;
+    private $stockModel;
+    private $imageHelper;
+    private $storeModel;
+    private $productCollectionFactory;
+
+    public function __construct(
+        //Framework\App\Action\Context $context,
+        Framework\Cache\Core $core,
+        Catalog\Model\Product $product,
+        CatalogInventory\Api\StockRegistryInterface $stockRegistryInterface,
+        Catalog\Helper\Image $image,
+        Store\Model\StoreManagerInterface $storeManagerInterface,
+        Reviews\Helper\Config $config,
+        //\Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+     )
+    {
+        // parent::__construct($context);
+
+        $this->configHelper = $config;
+        $this->cache = $core;
+        $this->productModel = $product;
+        $this->stockModel = $stockRegistryInterface;
+        $this->imageHelper = $image;
+        $this->storeModel = $storeManagerInterface;
+        $this->productCollectionFactory = $productCollectionFactory;
+    }
+
+    private function getProductCollection()
+    {
+        $collection = $this->productCollectionFactory->create();
+            /* Addtional */
+            $collection
+                ->addMinimalPrice()
+                ->addFinalPrice()
+                ->addTaxPercents()
+                ->addAttributeToSelect('*')
+                ->addUrlRewrite();
+            return $collection;
+    }
+
+    public function execute()
+    {
+        // Set timelimit to 0 to avoid timeouts when generating feed.
+        ob_start();
+        set_time_limit(0);
+
+        $store = $this->storeModel->getStore();
+
+        $productFeedEnabled = $this->configHelper->isProductFeedEnabled($store->getId());
+        if ($productFeedEnabled) {
+            // TODO:- Implement caching of Feed
+            $productFeed = "<?xml version='1.0'?>
+                    <rss version ='2.0' xmlns:g='http://base.google.com/ns/1.0'>
+                    <channel>
+                    <title><![CDATA[" . $store->getName() . "]]></title>
+                    <link>" . $store->getBaseUrl() . "</link>";
+
+            $products = $this->getProductCollection();
+
+            foreach ($products as $product) {
+                // Load image url via helper.
+                $imageUrl = $this->imageHelper->init($product, 'product_page_image_large')->getUrl();
+
+                $brand = $product->hasData('manufacturer') ? $product->getAttributeText('manufacturer') : ($product->hasData('brand') ? $product->getAttributeText('brand') : 'Not Available');
+                $price = $product->getPrice();
+                $finalPrice = $product->getFinalPrice();
+                $productFeed .= "<item>
+                        <g:id><![CDATA[" . $product->getSku() . "]]></g:id>
+                        <title><![CDATA[" . $product->getName() . "]]></title>
+                        <link><![CDATA[" . $product->getProductUrl() . "]]></link>
+                        <g:price>" . (!empty($price) ? number_format($price, 2) . " " . $store->getCurrentCurrency()->getCode() : ''). "</g:price>
+                        <g:sale_price>" . (!empty($finalPrice) ? number_format($finalPrice, 2) . " " . $store->getCurrentCurrency()->getCode() : '') . "</g:sale_price>
+                        <description><![CDATA[]]></description>
+                        <g:condition>new</g:condition>
+                        <g:image_link><![CDATA[" . $imageUrl . "]]></g:image_link>
+                        <g:brand><![CDATA[" . $brand . "]]></g:brand>
+                        <g:mpn><![CDATA[" . ($product->hasData('mpn') ? $product->getData('mpn') : $product->getSku()). "]]></g:mpn>
+                        <g:gtin><![CDATA[" . ($product->hasData('gtin') ? $product->getData('gtin') : ($product->hasData('upc') ? $product->getData('upc') : '')). "]]></g:gtin>
+                        <g:product_type><![CDATA[" . $product->getTypeID() . "]]></g:product_type>
+                        <g:shipping>
+                        <g:country>UK</g:country>
+                        <g:service>Standard Free Shipping</g:service>
+                        <g:price>0 GBP</g:price>
+                        </g:shipping>";
+
+                $categoryCollection = $product->getCategoryCollection();
+                if (count($categoryCollection) > 0) {
+                    foreach ($categoryCollection as $category) {
+                        $productFeed .= "<g:google_product_category><![CDATA[" . $category->getName() . "]]></g:google_product_category>";
+                    }
+                }
+
+                $stock = $this->stockModel->getStockItem(
+                    $product->getId(),
+                    $product->getStore()->getWebsiteId()
+                );
+                if ($stock->getIsInStock()) {
+                    $productFeed .= "<g:availability>in stock</g:availability>";
+                } else {
+                    $productFeed .= "<g:availability>out of stock</g:availability>";
+                }
+
+                $productFeed .= "</item>";
+            }
+
+            $productFeed .= "</channel></rss>";
+
+            // TODO:- Implement caching of feed
+
+            print $productFeed;
+            // exit();
+        } else {
+            print "Product Feed is disabled.";
+        }
+    }
+}

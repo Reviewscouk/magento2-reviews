669dfc76c5fb6c98d4421e85db1048a3a907e0a3
aj2967 <aj2967@hotmail.com>
2023-06-23 16:52:28 +0100

    Use parent data for configurable and grouped products if not available.

--- Controller/Index/Feed.php
+++ Controller/Index/Feed.php
@@ -26,7 +26,9 @@ class Feed extends Framework\App\Action\Action
         Catalog\Helper\Image $image,
         Store\Model\StoreManagerInterface $storeManagerInterface,
         Reviews\Helper\Config $config,
-        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+        \Magento\ConfigurableProduct\Model\Product\Type\Configurable $configurableType,
+        \Magento\Framework\HTTP\Client\Curl $curl
      )
     {
         parent::__construct($context);
@@ -38,6 +40,8 @@ class Feed extends Framework\App\Action\Action
         $this->imageHelper = $image;
         $this->storeModel = $storeManagerInterface;
         $this->productCollectionFactory = $productCollectionFactory;
+        $this->configurableType = $configurableType;
+        $this->curl = $curl;
     }
 
     private function getProductCollection()
@@ -51,6 +55,41 @@ class Feed extends Framework\App\Action\Action
                 ->addAttributeToSelect('*')
                 ->addUrlRewrite();
             return $collection;
+    }
+
+    private function validateImageUrl($imageUrl)
+    {
+        $options = [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER => true,
+            CURLOPT_NOBODY => true,
+        ];
+
+        if(strpos($imageUrl, "Magento_Catalog/images/product/placeholder/image.jpg") !== false){
+            return false;
+        }
+
+        return true;
+    }
+
+    private function validateVariantUrl($url)
+    {
+        $options = [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER => true,
+            CURLOPT_NOBODY => true,
+        ];
+
+        try {
+            $this->curl->get($url);
+            $this->curl->setOptions($options);
+        
+            if ($this->curl->getStatus() == 200) {
+                return true;
+            }
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 
     public function execute()
@@ -73,23 +112,57 @@ class Feed extends Framework\App\Action\Action
             $products = $this->getProductCollection();
 
             foreach ($products as $product) {
-                // Load image url via helper.
-                $imageUrl = $this->imageHelper->init($product, 'product_page_image_large')->getUrl();
+                $parentProductId = null;
+                $groupedParentId = null;
+                $configurableParentId = null;
+                $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+                
+                if ($objectManager->create('Magento\GroupedProduct\Model\Product\Type\Grouped')->getParentIdsByChild($product->getId())) {
+                    $groupedParentId = $objectManager->create('Magento\GroupedProduct\Model\Product\Type\Grouped')->getParentIdsByChild($product->getId());
+                }
+                if ($objectManager->create('Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable')->getParentIdsByChild($product->getId())) {
+                    $configurableParentId = $objectManager->create('Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable')->getParentIdsByChild($product->getId());
+                }
 
+                $parentId = null;
+                $parentProduct = null;
+                
+                if (isset($groupedParentId[0])) {
+                    $parentId = $groupedParentId[0];
+                } else if (isset($configurableParentId[0])) {
+                    $parentId = $configurableParentId[0];
+                }
+
+                // Load image url via helper.
+                $productImageUrl = $this->imageHelper->init($product, 'product_page_image_large')->getUrl();
+                $imageLink = $productImageUrl;
+                $productUrl = $product->getProductUrl();
+                
+                if (isset($parentId)) {
+                    $parentProduct = $objectManager->create('Magento\Catalog\Model\Product')->load($parentId);
+                    
+                    $parentProductImageUrl = $this->imageHelper->init($parentProduct, 'product_page_image_large')->getUrl();
+                    $validVariantImage = $this->validateImageUrl($productImageUrl);
+                    if (!$validVariantImage) {
+                        $imageLink = $parentProductImageUrl;
+                    }
+
+                    $productUrl = $parentProduct->getProductUrl();
+                }
+                
                 $brand = $product->hasData('manufacturer') ? $product->getAttributeText('manufacturer') : ($product->hasData('brand') ? $product->getAttributeText('brand') : 'Not Available');
                 $price = $product->getPrice();
-
                 $finalPrice = $product->getFinalPrice();
 
                 $productFeed .= "<item>
                         <g:id><![CDATA[" . $product->getSku() . "]]></g:id>
                         <title><![CDATA[" . $product->getName() . "]]></title>
-                        <link><![CDATA[" . $product->getProductUrl() . "]]></link>
-                        <g:price>" . number_format($price, 2) . " " . $store->getCurrentCurrency()->getCode() . "</g:price>
-                        <g:sale_price>" . number_format($finalPrice, 2) . " " . $store->getCurrentCurrency()->getCode() . "</g:sale_price>
+                        <link><![CDATA[" . $productUrl . "]]></link>
+                        <g:price>" . (!empty($price) ? number_format($price, 2) . " " . $store->getCurrentCurrency()->getCode() : '') . "</g:price>
+                        <g:sale_price>" . (!empty($finalPrice) ? number_format($finalPrice, 2) . " " . $store->getCurrentCurrency()->getCode() : '') . "</g:sale_price>
                         <description><![CDATA[]]></description>
                         <g:condition>new</g:condition>
-                        <g:image_link><![CDATA[" . $imageUrl . "]]></g:image_link>
+                        <g:image_link><![CDATA[" . $imageLink . "]]></g:image_link>
                         <g:brand><![CDATA[" . $brand . "]]></g:brand>
                         <g:mpn><![CDATA[" . ($product->hasData('mpn') ? $product->getData('mpn') : $product->getSku()). "]]></g:mpn>
                         <g:gtin><![CDATA[" . ($product->hasData('gtin') ? $product->getData('gtin') : ($product->hasData('upc') ? $product->getData('upc') : '')). "]]></g:gtin>
@@ -118,6 +191,7 @@ class Feed extends Framework\App\Action\Action
                 }
 
                 $productFeed .= "</item>";
+                $parentProduct = null;
             }
 
             $productFeed .= "</channel></rss>";

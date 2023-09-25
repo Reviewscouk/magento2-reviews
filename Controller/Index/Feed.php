d192278835f4b8300a56841ece518bebce74a2e0
Konrad Siamro <k.siamro@netz98.de>
2023-09-25 16:41:28 +0200

    Another refactor for Feed Controlelr

--- Controller/Index/Feed.php
+++ Controller/Index/Feed.php
@@ -7,15 +7,15 @@
 
 namespace Reviewscouk\Reviews\Controller\Index;
 
+use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductRepositoryInterfaceFactory;
 use Magento\Catalog\Helper\Image;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
 use Magento\Catalog\Model\ResourceModel\Product\Collection;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\CatalogInventory\Api\StockRegistryInterface;
 use Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable as ConfigurableTypeResourceModel;
-use Magento\Framework;
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\GroupedProduct\Model\Product\Type\Grouped;
 use Magento\Store\Model\StoreManagerInterface;
@@ -38,6 +38,7 @@ class Feed implements HttpGetActionInterface
      * @param Grouped                           $groupedProductModel
      * @param ConfigurableTypeResourceModel     $configurableProductModel
      * @param ProductRepositoryInterfaceFactory $productRepositoryFactory
+     * @param CategoryCollectionFactory         $categoryCollectionFactory
      */
     public function __construct(
         private readonly StockRegistryInterface            $stockModel, // StockRegistryInterface is deprecated.
@@ -48,7 +49,8 @@ class Feed implements HttpGetActionInterface
         private readonly ResultFactory                     $resultFactory,
         private readonly Grouped                           $groupedProductModel,
         private readonly ConfigurableTypeResourceModel     $configurableProductModel,
-        private readonly ProductRepositoryInterfaceFactory $productRepositoryFactory
+        private readonly ProductRepositoryInterfaceFactory $productRepositoryFactory,
+        private readonly CategoryCollectionFactory         $categoryCollectionFactory
     ) {
     }
 
@@ -71,18 +73,20 @@ class Feed implements HttpGetActionInterface
         set_time_limit(0);
         // Basically not good solution. Even with that, timeout can be thrown with many products.
         // Consider using pagination as for API Controller and remove the ob_start and set_time_limit
+        // Eventually create cron, that would create the Feed in the background and the controller would only display it
 
         // TODO:- Implement caching of Feed
         $productFeed = "<?xml version='1.0'?>
-                <rss version ='2.0' xmlns:g='http://base.google.com/ns/1.0'>
-                <channel>
-                <title><![CDATA[" . $store->getName() . "]]></title>
-                <link>" . $store->getBaseUrl() . "</link>";
+<rss version ='2.0' xmlns:g='http://base.google.com/ns/1.0'>
+    <channel>
+        <title><![CDATA[" . $store->getName() . "]]></title>
+        <link>" . $store->getBaseUrl() . "</link>";
 
         $page = 0;
         do {
             $productCollection = $this->getProductCollection($page);
 
+            /** @var ProductInterface $product */
             foreach ($productCollection as $product) {
 
                 $parentId = null;
@@ -129,55 +133,70 @@ class Feed implements HttpGetActionInterface
                 $price = $product->getPrice();
                 $finalPrice = $product->getFinalPrice();
 
-                $productFeed .= "<item>
-                    <g:id><![CDATA[" . $product->getSku() . "]]></g:id>
-                    <title><![CDATA[" . $product->getName() . "]]></title>
-                    <link><![CDATA[" . $productUrl . "]]></link>
-                    <g:price>" . (!empty($price) ? number_format($price, 2) . " " . $store->getCurrentCurrency()->getCode() : '') . "</g:price>
-                    <g:sale_price>" . (!empty($finalPrice) ? number_format($finalPrice, 2) . " " . $store->getCurrentCurrency()->getCode() : '') . "</g:sale_price>
-                    <description><![CDATA[]]></description>
-                    <g:condition>new</g:condition>
-                    <g:image_link><![CDATA[" . $imageLink . "]]></g:image_link>
-                    <g:brand><![CDATA[" . $brand . "]]></g:brand>
-                    <g:mpn><![CDATA[" . ($product->hasData('mpn') ? $product->getData('mpn') : $product->getSku()) . "]]></g:mpn>
-                    <g:gtin><![CDATA[" . ($product->hasData('gtin') ? $product->getData('gtin') : ($product->hasData('upc') ? $product->getData('upc') : '')) . "]]></g:gtin>
-                    <g:product_type><![CDATA[" . $product->getTypeID() . "]]></g:product_type>
-                    <g:shipping>
-                    <g:country>UK</g:country>
-                    <g:service>Standard Free Shipping</g:service>
-                    <g:price>0 GBP</g:price>
-                    </g:shipping>";
+                // I dont think, UK should be hardcoded as Shipping.
+                $productFeed .= "
+        <item>
+            <g:id><![CDATA[" . $product->getSku() . "]]></g:id>
+            <title><![CDATA[" . $product->getName() . "]]></title>
+            <link><![CDATA[" . $productUrl . "]]></link>
+            <g:price>" . (!empty($price) ? number_format($price, 2) . " " . $store->getCurrentCurrency()->getCode() : '') . "</g:price>
+            <g:sale_price>" . (!empty($finalPrice) ? number_format($finalPrice, 2) . " " . $store->getCurrentCurrency()->getCode() : '') . "</g:sale_price>
+            <description><![CDATA[]]></description>
+            <g:condition>new</g:condition>
+            <g:image_link><![CDATA[" . $imageLink . "]]></g:image_link>
+            <g:brand><![CDATA[" . $brand . "]]></g:brand>
+            <g:mpn><![CDATA[" . ($product->hasData('mpn') ? $product->getData('mpn') : $product->getSku()) . "]]></g:mpn>
+            <g:gtin><![CDATA[" . ($product->hasData('gtin') ? $product->getData('gtin') : ($product->hasData('upc') ? $product->getData('upc') : '')) . "]]></g:gtin>
+            <g:product_type><![CDATA[" . $product->getTypeID() . "]]></g:product_type>
+            <g:shipping>
+            <g:country>UK</g:country>
+            <g:service>Standard Free Shipping</g:service>
+            <g:price>0 GBP</g:price>
+            </g:shipping>";
 
-                $categoryCollection = $product->getCategoryCollection();
-                if (count($categoryCollection) > 0) {
+                // If You really need to provide also Category names, the Category collection have to be loaded as well.
+                // It is not very optimized for many products.
+                $categoryIds = $product->getCategoryIds();
+                try {
+                    $categoryCollection = $this->categoryCollectionFactory->create()
+                        ->addAttributeToSelect(['name'])
+                        ->addAttributeToFilter('entity_id', $categoryIds);
+                } catch (\Exception $e) {
+                    $categoryCollection = null;
+                }
+
+                if (!is_null($categoryCollection) && count($categoryCollection) > 0) {
                     foreach ($categoryCollection as $category) {
                         $productFeed .= sprintf(
-                            "<g:google_product_category><![CDATA[%s]]></g:google_product_category>",
+                            "\n\t\t\t<g:google_product_category><![CDATA[%s]]></g:google_product_category>",
                             $category->getName()
                         );
                     }
                 }
 
-                // The StockRegistryInterface is deprecated.
+                // The StockRegistryInterface is deprecated. Implemented logic will not reflect real Stock Status.
                 // See https://developer.adobe.com/commerce/php/development/components/web-api/inventory-management/
+                // For quicker resolve, consider using $product->isSalable() instead $stock->getIsInStock()
+                // Otherwise if Shop uses MSI load StockInventories and check availability there
                 $stock = $this->stockModel->getStockItem(
                     $product->getId(),
                     $product->getStore()->getWebsiteId()
                 );
                 if ($stock->getIsInStock()) {
-                    $productFeed .= "<g:availability>in stock</g:availability>";
+                //if ($product->isSalable()) {
+                    $productFeed .= "\n\t\t\t<g:availability>in stock</g:availability>";
                 } else {
-                    $productFeed .= "<g:availability>out of stock</g:availability>";
+                    $productFeed .= "\n\t\t\t<g:availability>out of stock</g:availability>";
                 }
 
-                $productFeed .= "</item>";
+                $productFeed .= "\n\t\t</item>";
                 $parentProduct = null;
             }
 
             $page++;
         } while ($productCollection->count());
 
-        $productFeed .= "</channel></rss>";
+        $productFeed .= "\n\t</channel>\n</rss>";
 
         // TODO:- Implement caching of feed
         $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
@@ -199,13 +218,15 @@ class Feed implements HttpGetActionInterface
     {
         $collection = $this->productCollectionFactory->create();
 
+        // If You want to use Collection, do not load all Attributes if not required. Especially with big collection.
+        // Add just required attributes.
         $collection
             ->addMinimalPrice()
             ->addFinalPrice()
             ->addTaxPercents()
-            ->addAttributeToSelect('*')
+            ->addAttributeToSelect(['name', 'manufacturer', 'gtin', 'brand', 'image'])
             ->addUrlRewrite()
-            ->setPageSize(100)
+            ->setPageSize(2)
             ->setCurPage($page);
 
         return $collection;
